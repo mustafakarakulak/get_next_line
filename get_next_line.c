9ebbf6b498d15470438043249aef09edecf6e707
Mustafa Karakulak <mustafa.monster46@gmail.com>
2023-01-04 22:01:22 +0300

    trainers

---
+++ get_next_line.c
@@ -0,0 +1,41 @@
+#include "get_next_line.h"
+
+char	*ft_read(int fd, char *str)
+{
+	int		rd;
+	char	*buff;
+
+	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buff)
+		return (NULL);
+	rd = 1;
+	while (!ft_n(str) && rd != 0)
+	{
+		rd = read(fd, buff, BUFFER_SIZE);
+		if (rd == -1)
+		{
+			free(str);
+			free(buff);
+			return (NULL);
+		}
+		*(buff + rd) = '\0';
+		str = ft_strjoin(str, buff);
+	}
+	free(buff);
+	return (str);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*str;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	str = ft_read(fd, str);
+	if (!str)
+		return (NULL);
+	line = ft_get_line(str);
+	str = ft_update_str(str);
+	return (line);
+}
